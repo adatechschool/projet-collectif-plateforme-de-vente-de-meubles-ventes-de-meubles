974d1e43044d135025e72b80b36b89b0b250c85a
arthur MICHAILESCO <arthur.michailesco@gmail.com>
2024-03-04 12:03:09 +0100

    first push

---
+++ backmeubles/src/test/java/com/meubles/backmeubles/BackmeublesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meubles.backmeubles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackmeublesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
