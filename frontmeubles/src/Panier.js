abfdf7f1435aeeed789d78a66590b6500086b888
KhalilHouni <132214784+KhalilHouni@users.noreply.github.com>
2024-03-06 15:11:24 +0100

    Update Panier.js

--- frontmeubles/src/Panier.js
+++ frontmeubles/src/Panier.js
@@ -4,14 +4,9 @@ import React from "react";
 import './Panier.css';
 
 const Panier = () => {
-  const userFurniture = [
-    { image: 'table.png', name: 'Meuble 1', price: 100 },
-    { image: 'chaisegamer.jpeg', name: 'Meuble 2', price: 150 },
-    { image: 'lampe.webp', name: 'Meuble 3', price: 200 },
-    // Ajoutez autant d'éléments que nécessaire
-  ];
+ 
 
-  // Le reste de votre composant...
+  // Le reste va ici car la page est vide...
 
   return (
     <div>
