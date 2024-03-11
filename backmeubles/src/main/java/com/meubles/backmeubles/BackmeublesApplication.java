fbe8c0cac5d875e496ac4a8027fe74ead0647446
KhalilHouni <132214784+KhalilHouni@users.noreply.github.com>
2024-03-11 01:50:51 +0100

    disabled cors for access to login
    corrected a small mistake in folder path

--- backmeubles/src/main/java/com/meubles/backmeubles/BackmeublesApplication.java
+++ backmeubles/src/main/java/com/meubles/backmeubles/BackmeublesApplication.java
@@ -2,12 +2,13 @@ package com.meubles.backmeubles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.meubles.backmeubles")
 public class BackmeublesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackmeublesApplication.class, args);
 	}
-
 }
--- backmeubles/src/main/java/com/meubles/backmeubles/controller/UserController.java
+++ backmeubles/src/main/java/com/meubles/backmeubles/controller/UserController.java
@@ -10,7 +10,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
-
 public class UserController {
 
     private final UserService userService;
@@ -19,14 +18,16 @@ public class UserController {
     public User create(@RequestBody User user) {
         return userService.creer(user);
     }
-    @GetMapping("/read")
-    public List<User> read(@RequestParam(name = "nomTriAlphabetique", required = false) String sortParam) {
+
+    @PostMapping("/read")
+    public List<User> read(@RequestBody(required = false) String sortParam) {
         List<User> users = userService.lire();
         if (sortParam != null && sortParam.equals("nomTriAlphabetique")) {
             users.sort(Comparator.comparing(User::getNom));
         }
         return users;
     }
+
     @PutMapping("/update/{id}")
     public User update(@PathVariable Integer id, @RequestBody User user) {
         return userService.modifier(id, user);
