6360178f47c65a37a1f2a5534169b78580502841
clara <clara.jacot@gmail.com>
2024-03-12 11:37:09 +0100

    profile toujour

--- frontmeubles/src/pages/Profile.js
+++ frontmeubles/src/pages/Profile.js
@@ -9,7 +9,10 @@ import axios from 'axios';
 const Profile = () =>{
 
   const [Data, setData] = useState([]);
-  const [newFurniture, setNewFurniture] = useState({ nom: '', prix: ''});
+  const [newFurniture, setNewFurniture] = useState({ nom: '', prix: '',description:'', dimension: '',matieres:''});
+  const [matieres,setMatieres] = useState('');
+  
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -28,6 +31,7 @@ const Profile = () =>{
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(newFurniture);
+    console.log(newFurniture.matieres)
     axios.post("/meubles/create", newFurniture)
     .then(function (response) {
       console.log(response);
@@ -39,8 +43,14 @@ const Profile = () =>{
     console.log(newFurniture);
     alert('Soumission envoyée !');
     // Réinitialiser le formulaire après la soumission
-    setNewFurniture({ nom: '', prix: '' });
+    setNewFurniture({ nom: '', prix: '',description:'',dimension:'', matieres:'' });
   };
+
+  function test(truc){
+    console.log(truc);
+    console.log(truc.target.value)
+    setNewFurniture({matieres:(truc.target.value)});
+  }
   return (
     <section>
       <h2>Meubles mis en vente</h2>
@@ -67,23 +77,88 @@ const Profile = () =>{
               <input
                 type="text"
                 name="prix"
-                placeholder="price"
+                placeholder="prix"
                 value={newFurniture.price}
                 onChange={handleChange}
                 required
               />
+              <input
+              type='text'
+              name='description'
+              placeholder='Courte description'
+              value={newFurniture.description}
+              onChange={handleChange}
+              />
+              <input
+              type='text'
+              name='dimension'
+              placeholder='Dimensions'
+              value={newFurniture.dimension}
+              onChange={handleChange}
+              />
+              <span>Matière</span>
+              <select value={newFurniture.matieres} onChange={e => test(e)}>
+                <option value="Plastique">Plastique</option>
+                <option value="Osier">Osier</option>
+                <option value="Rotin">Rotin</option>
+                <option value="Pierre">Pierre</option>
+                <option value="Verre">Verre</option>
+                <option value="Métal">Métal</option>
+                <option value="Bois_récupéré">Bois récupéré</option>
+                <option value="Bois_recyclé">Bois recyclé</option>
+                <option value="Contreplaqué">Contreplaqué</option>
+                <option value="Formica">Formica</option>
+                <option value="MDF">MDF</option>
+                <option value="Bois_de_placage">Bois de placage</option>
+              </select>
+              <span>Couleurs</span>
+              <select value={newFurniture.couleurs} onChange={e => setNewFurniture(e.target.value)}>
+                  <option value="noir">Noir</option>
+                  <option value="pourpre">Pourpre</option>
+                  <option value="rouge">Rouge</option>
+                  <option value="orange">Orange</option>
+                  <option value="jaune">Jaune</option>
+                  <option value="vert">Vert</option>
+                  <option value="bleu">Bleu</option>
+                  <option value="violet">Violet</option>
+                  <option value="ivoire">Ivoire</option>
+                  <option value="crème">Crème</option>
+                  <option value="beige">Beige</option>
+                  <option value="rose">Rose</option>
+                  <option value="kaki">Kaki</option>
+                  <option value="brun">Brun</option>
+                  <option value="marron">Marron</option>
+                  <option value="bordeaux">Bordeaux</option>
+            </select>
+            <span>Type</span>
+            <select value={newFurniture.type} onChange={e => setNewFurniture(e.target.value)}>
+                <option value="canape">Canapé</option>
+                <option value="fauteuil">Fauteuil</option>
+                <option value="table">Table</option>
+                <option value="chaise">Chaise</option>
+                <option value="lit">Lit</option>
+                <option value="armoire">Armoire</option>
+                <option value="commode">Commode</option>
+                <option value="buffet">Buffet</option>
+                <option value="bibliotheque">Bibliothèque</option>
+                <option value="etagere">Etagère</option>
+                <option value="bureau">Bureau</option>
+                <option value="table_de_travail">Table de travail</option>
+                <option value="meuble_tv">Meuble TV</option>
+                <option value="banc">Banc</option>
+                <option value="tabouret">Tabouret</option>
+                <option value="pouf">Pouf</option>
+                <option value="coiffeuse">Coiffeuse</option>
+                <option value="vaisselier">Vaisselier</option>
+                <option value="pouf">Pouf</option>
+                <option value="meuble_de_salle_de_bain">Meuble de salle de bain</option>
+                <option value="meuble_de_cuisine">Meuble de cuisine</option>
+              </select>
               <button type="submit">Soumettre le meuble</button>
             </form>
           </div>
     </section>
   )
-  // var a = userFurniture();
-  // var meubles = [];
-  // meubles.push(a);
-  // console.log(a);
-
-
-  // État pour la soumission d'un nouveau meuble
  
 
 };
