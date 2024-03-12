3facc7ef63400e740a18b0198944d721504074a6
Khalil Houni <132214784+KhalilHouni@users.noreply.github.com>
2024-03-12 11:24:16 +0100

    Delete backmeubles directory

--- backmeubles/src/main/java/com/meubles/backmeubles/repositoryBdd/RepositoryMeuble.java
+++
@@ -1,9 +0,0 @@
-package com.meubles.backmeubles.repositoryBdd;
-
-
-import com.meubles.backmeubles.bdd.Meuble;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryMeuble extends JpaRepository<Meuble,Integer >{
-
-}
