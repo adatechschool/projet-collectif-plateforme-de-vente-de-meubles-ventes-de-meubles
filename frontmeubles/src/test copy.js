6194a60e5f5f4f2e8fd728d54453259949a79676
Khalil Houni <132214784+KhalilHouni@users.noreply.github.com>
2024-03-13 21:53:12 +0100

    Delete frontmeubles directory

--- frontmeubles/src/test copy.js
+++
@@ -1,6 +0,0 @@
-async function afficherUsers() {
-    const reponse = await fetch("http://localhost:8080/users/read ");
-    const users = await reponse.json();
-    console.log(users);
-  };
-afficherUsers();
